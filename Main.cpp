b883443b1eb93e00030c5c4bc551decb7b08a7e9
Nik Zandian <nik@quietrocket.com>
2022-05-02 22:59:51 -0700

    Cleaned up main

--- Main.cpp
+++ Main.cpp
@@ -3,29 +3,14 @@
 
 #include "Mouse.h"
 
+int main(int argc, char *argv[])
+{
+    Mouse m { NORTH };
 
-// namespace MMS {
-
-
-//     void turnLeft(&Direction d)
-
-
-// };
-
-int main(int argc, char* argv[]) {
-
-
-    std::vector<int> v;
-
-    Mouse m{NORTH};
-
-    log("This is our version of the code");
     log("Running...");
-    API::setColor(0, 0, 'G');
-    API::setText(0, 0, "abc");
 
-
-    while (true) {
+    while (true)
+    {
         m.step();
     }
 }
